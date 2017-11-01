586a9d2867825159bc489af6d3db7aee5753b044
lafitehhq <lafitehhq@126.com>
2017-11-01 19:51:59 +0800

    用slate库读取PDF

--- Python-03数据处理/Reference/code/chp5-pdf-处理PDF文件/parse_pdf.py
+++ Python-03数据处理/Reference/code/chp5-pdf-处理PDF文件/parse_pdf.py
@@ -1,13 +1,20 @@
 #-*-coding:utf-8_-*-
 
-# Todo
+"""
+Description:
+用slate库读取PDF
 
-import slate
+拓展:
 
-pdf = '/Users/lafitehhq/PycharmProjects/PythonBook/Python-03数据处理/Reference/data/chp5/en-final-table9.txt'
 
-with open(pdf) as f:
-    doc = slate.PDF(f)
+"""
 
-for page in doc[:2]:
-    print type(page)
+import slate  # 导入slate库。
+
+pdf = 'E:\Pycharm Workingspace\PythonBook\Python-03数据处理\Reference\data\chp5\EN-FINAL Table 9.pdf'  # 创建字符串变量，用于保存文件路径，一定要确保空格和大小写都正确
+
+with open(pdf) as f:  # 将文件名字符串传入Python的 open函数
+    doc = slate.PDF(f)  # 将打开的文件 f 传递给slate.PDF(f)，slate可以将PDF文件解析成可用的格式
+
+for page in doc[:2]:  # 遍历文档doc的前两页并输出
+    print page
