750059667f8a29b12879dbc4d181684e9f394d4e
lafitehhq <lafitehhq@126.com>
2017-11-01 10:56:33 +0800

    读取csv文件

--- Python-03数据处理/Reference/code/chp3-csv-json-xml-处理csv-json-xml文件/import_csv_data.py
+++ Python-03数据处理/Reference/code/chp3-csv-json-xml-处理csv-json-xml文件/import_csv_data.py
@@ -1,9 +1,15 @@
 #--coding:utf-8--
 
+"""
+
+拓展：  
+open()函数：https://docs.python.org/2/library/functions.html#open
+"""
+
 import csv
 
-csvfile = open('../../data/chp3/data-text.csv', 'rb')
-reader = csv.reader(csvfile)
+csvfile = open('../../data/chp3/data-text.csv', 'rb')  # open()是Python 的内置函数；'rb'以只读方式和二进制方式打开文件；'wb'，后者表示以二进制方式写入
+reader = csv.reader(csvfile)  # 将csvfile传递给csv模块的reader函数，让csv模块将打开的文件当作CSV 来读取
 
-for row in reader:
+for row in reader:  # reader对象是一个保存数据行的 Python容器
     print row
