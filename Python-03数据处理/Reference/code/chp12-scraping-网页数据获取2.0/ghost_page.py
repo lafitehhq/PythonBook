f81585479fb3859dfd818fccd43a64e50f6f81dc
lafitehhq <lafitehhq@126.com>
2017-11-09 18:43:36 +0800

    2017-11-09

--- Python-03数据处理/Reference/code/chp12-scraping-网页数据获取2.0/ghost_page.py
+++ Python-03数据处理/Reference/code/chp12-scraping-网页数据获取2.0/ghost_page.py
@@ -1,24 +1,30 @@
+# -*- coding: utf-8 -*-
+
 from ghost import Ghost
 from time import sleep
 
-ghost = Ghost()
+ghost = Ghost()  # 码调用 Ghost类的会话对象，实例化一个Ghost 对象来同页面交互。
 
-with ghost.start(viewport_size=(1375,769)) as session:
-    page, extra_resources = session.open('http://python.org')
+with ghost.start(viewport_size=(1375, 769)) as session:
+    page, extra_resources = session.open('http://python.org')  # Ghost 类的 open 方法返回两个对象，所以这行代码在两个独立的变量中捕获这些对象。第一个对象是用来同 HTML 元素交互的页面对象。第二个对象是页面加载的其他资源列表（你在网络标签中看到的列表）。
 
     print page
     print page.url
+    print page.headers
+    print page.http_status
+    print page.content  # 页面对象有很多属性，比如头部、内容、链接和页面上的内容。这行代码打印页面的内容
 
     for r in extra_resources:
-        print r.url
+        print r.url  # 遍历页面的其他资源，并且打印它们，来看是否有用。有时，这些 URL 是API调用，可以利用它们简化数据的访问
 
-    print page.content.contains('input')
+    print page.content.contains('input')  # 测试页面上是否存在一个 input 标签（大多数的搜索框是简单的输入对象）。这会返回一个布尔值。
     result, resources = session.evaluate(
-        'document.getElementsByTagName("input")')
-    print result.get('length')
+        'document.getElementsByTagName("input")')  #  使用一些简单的JavaScript 来找到页面上所有以“input”为标签名称的元素。
+    print result.get('length')  # 打印来看响应中的JavaScript 数组的长度。
+
 
     result, resources = session.evaluate(
-        'document.getElementsByTagName("input")[0].getAttribute("id");')
+        'document.getElementsByTagName("input")[0].getAttribute("id");')  # 索引结果列表，获取id 属性。JavaScript直接给出了元素的CSS 属性，所以这是一个查看选择元素的相关 CSS的有用方式。
     print result
 
     result, resources = session.set_field_value("input", "scraping")
--- Python-03数据处理/Reference/code/chp12-scraping-网页数据获取2.0/selenium_form_submit.py
+++ Python-03数据处理/Reference/code/chp12-scraping-网页数据获取2.0/selenium_form_submit.py
@@ -1,28 +1,58 @@
-from selenium import webdriver
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver  # 导入来自Selenium的 webdriver 模块。这个模块用来调用任何已经安装的驱动器。
 from time import sleep
 
+# 获得一个页面加载完的浏览器对象（browser 变量）
+browser = webdriver.Firefox()  #  通过使用 webdriver 模块的 Firefox 类初始化 Firefox 浏览器对象。这会在计算机上打开一个新的浏览器窗口。
+browser.get('http://www.fairphone.com/we-are-fairphone/')  # 通过 get 方法和一个 URL 参数，访问想要抓取的 URL。打开的浏览器应该开始加载页面了
+browser.maximize_window()  # 使用 maximize_browser 方法最大化打开的浏览器。这会帮助 Selenium“看到”更多的内容。
 
-browser = webdriver.Firefox()
-browser.get('http://google.com')
 
-inputs = browser.find_elements_by_css_selector('form input')
-for i in inputs:
-    if i.is_displayed():
-        search_bar = i
-        break
+content = browser.find_element_by_css_selector('div.content')  # browser 对象有一个函数 find_element_by_css_selector，使用 CSS 选择器来选择HTML 对象。这行代码选择了第一个类名称为 content 的 div，这会返回第一个匹配的对象（一个 HTMLElement 对象）。
 
-search_bar.send_keys('web scraping with python')
+print content.text  # 打印第一个匹配对象的文本内容。
 
-search_button = browser.find_element_by_css_selector('form button')
-search_button.click()
+all_bubbles = browser.find_elements_by_css_selector('div.content')  # 码使用 find_elements_by_css_selector 方法，传递一个 CSS 选择器，找到所有匹配的对象。这个方法返回一个 HTMLElement 对象列表。
 
-browser.implicitly_wait(10)
-results = browser.find_elements_by_css_selector('div h3 a')
+print len(all_bubbles)
 
-for r in results:
-    action = webdriver.ActionChains(browser)
-    action.move_to_element(r)
-    action.perform()
-    sleep(2)
+for bubble in all_bubbles:  # 遍历列表，并且打印每一个对象的内容。
+    print bubble.text
 
-browser.quit()
+iframe = browser.find_element_by_xpath('//iframe')  # 使用find_element_by_xpath方法，返回第一个匹配iframe 标签的元素。
+
+new_url = iframe.get_attribute('src')  # 得到src属性，这包含在 iframe中加载的页面的 URL。
+
+browser.get(new_url)  # 在浏览器中加载 iframe的URL。
+
+# 找到了如何加载想要的内容的方法现在看一下是否可以加载所有的内容气泡：
+
+all_bubbles = browser.find_elements_by_css_selector('div.content')
+
+for elem in all_bubbles:
+    print elem.text
+
+# browser.get('http://baidu.com')
+#
+# inputs = browser.find_elements_by_css_selector('form input')
+# for i in inputs:
+#     if i.is_displayed():
+#         search_bar = i
+#         break
+#
+# search_bar.send_keys('web scraping with python')
+#
+# search_button = browser.find_element_by_css_selector('form button')
+# search_button.click()
+#
+# browser.implicitly_wait(10)
+# results = browser.find_elements_by_css_selector('div h3 a')
+#
+# for r in results:
+#     action = webdriver.ActionChains(browser)
+#     action.move_to_element(r)
+#     action.perform()
+#     sleep(2)
+#
+# browser.quit()
